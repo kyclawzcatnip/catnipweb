c33d8b35739e6df39722da06f2d4e24c45cd8f30
Kyan Castro <kyancas@Kyans-MacBook-Pro.local>
2026-07-21 00:41:37 -0600

    Force display property on section navigation to guarantee all tabs switch

--- script.js
+++ script.js
@@ -341,8 +341,10 @@ document.addEventListener('DOMContentLoaded', () => {
       const idMatches = section.getAttribute('id') === `${sectionId}-section`;
       if (idMatches) {
         section.classList.add('active');
+        section.style.setProperty('display', 'block', 'important');
       } else {
         section.classList.remove('active');
+        section.style.setProperty('display', 'none', 'important');
       }
     });
 
